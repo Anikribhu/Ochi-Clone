5548e7ef3231bb44935fc346bd3d82acc081591d
AnikRibhu <anikmallick28@gmail.com>
2024-06-28 11:50:48 +0530

    Feature Section Updated

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -18,12 +18,13 @@ function Featured() {
             </div>
             <div className="px-20 max-sm:px-10">
                 <div className="cards w-full  max-sm:flex-col max-md:flex-col flex gap-10 pt-10">
-                    <motion.div onHoverStart={()=>handleHover(0)} onHoverEnd={()=>handleHoverEnd(0)} className="card-container  relative rounded-2xl w-1/2 h-[75vh] max-sm:h-[40vh] max-md:h-[60vh] max-sm:w-full max-md:w-full bg-slate-200">
-                        <h1 className='text-[#CDEA68] overflow-hidden flex absolute left-full max-sm:left-52 max-md:left-72 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10  tracking-tight leading-none  text-8xl'>{"FYDE".split('').map((item, index) =>
+                    <motion.div onHoverStart={()=>handleHover(0)} onHoverEnd={()=>handleHoverEnd(0)} className="card-container m-2  relative rounded-2xl w-1/2  max-sm:h-[40vh] max-md:h-[60vh] max-sm:w-full max-md:w-full bg-slate-200">
+                        <h1 className='text-[#CDEA68] overflow-hidden flex absolute left-full max-sm:left-52 max-md:left-72 -translate-y-1/2 -translate-x-60 top-1/2 z-10  tracking-tight leading-none  text-8xl'>{"GEMINI".split('').map((item, index) =>
                             <motion.span initial={{y:"100%"}} animate={cards[0]} transition={{ease:[0.83, 0, 0.17, 1],delay:index*.06}}  className='inline-block' key={item}>{item}</motion.span>)}</h1>
-                        <div className="w-full h-full  rounded-2xl scale-50 overflow-hidden">
-                            <img className='w-full h-full  bg-cover bg-center' src="https://ochi.design/wp-content/uploads/2023/10/Fyde_Illustration_Crypto_2-663x551.png" alt="" />
-                        </div>
+                        {/* <div className="w-full h-full  rounded-2xl scale-50 overflow-hidden">
+                            
+                        </div> */}
+                        <img className='w-full h-full  bg-cover bg-center' src='src\assets\gemini clone.png' alt="" />
                     </motion.div>
 
                     <motion.div onHoverStart={()=>handleHover(1)} onHoverEnd={()=>handleHoverEnd(1)}  className="card-container  relative rounded-2xl w-1/2 h-[75vh] max-sm:h-[40vh] max-md:h-[60vh] max-sm:w-full max-md:w-full">
